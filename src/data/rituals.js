ef2eddb2dc95bce35c632704dc2ddc4730c6fb07
Olga Ingelaere <113905169+OlyaIngelaere@users.noreply.github.com>
2025-11-23 23:17:05 +0100

    Add extra product pictures

--- src/data/rituals.js
+++ src/data/rituals.js
@@ -83,49 +83,39 @@ export const rituals = {
     title: "Your Calm Ritual",
     description:
       "Soothe sensitivity and restore harmony with gentle, restorative care.",
-    morningProducts: [
+      morningProducts: [
       {
-        img: "products/soothing-gel-cleanse.jpg",
-        name: "Soothing Gel Cleanse",
-        price: "$27",
+        img: "products/relief_sun.jpg",
+        name: "Relief Sun : Rice + Probiotics",
+        price: "$28",
       },
       {
-        img: "products/calming-mist.jpg",
-        name: "Calming Mist",
+        img: "products/revive_serum.jpg",
+        name: "Revive Serum : Ginseng + Snail Mucin",
         price: "$32",
       },
       {
-        img: "products/centella-serum.jpg",
-        name: "Centella Serum",
-        price: "$48",
-      },
-      {
-        img: "products/barrier-cream.jpg",
-        name: "Barrier Cream",
-        price: "$38",
+        img: "products/dynasty-cream.jpg",
+        name: "Dynasty Cream",
+        price: "$45",
       },
     ],
     eveningProducts: [
       {
-        img: "products/gentle-cleanse.jpg",
-        name: "Gentle Cleanse",
-        price: "$28",
+        img: "products/calming_serum.jpg",
+        name: "Calming Serum Green Tea+Panthenol",
+        price: "$38",
       },
       {
-        img: "products/soothing-pad.jpg",
-        name: "Soothing Pad",
+        img: "products/glow_deep_serum.jpg",
+        name: "Glow Deep Serum : Rice + Alpha-Arbutin",
+        price: "$30",
+      },
+      {
+        img: "products/rice_milk.jpg",
+        name: "Glow Replenishing Rice Milk",
         price: "$25",
-      },
-      {
-        img: "products/recovery-serum.jpg",
-        name: "Recovery Serum",
-        price: "$50",
-      },
-      {
-        img: "products/sleeping-mask.jpg",
-        name: "Sleeping Mask",
-        price: "$42",
-      },
+      }
     ],
   },
 
@@ -133,49 +123,39 @@ export const rituals = {
     title: "Your Balancing Ritual",
     description:
       "Harmonize your skin’s natural balance with clarifying, refining formulas.",
-    morningProducts: [
+morningProducts: [
       {
-        img: "products/balancing-foam.jpg",
-        name: "Balancing Foam",
-        price: "$26",
-      },
-      {
-        img: "products/ph-toner.jpg",
-        name: "pH Toner",
-        price: "$30",
-      },
-      {
-        img: "products/pore-serum.jpg",
-        name: "Pore Serum",
-        price: "$48",
-      },
-      {
-        img: "products/light-gel.jpg",
-        name: "Lightweight Gel",
-        price: "$35",
-      },
-    ],
-    eveningProducts: [
-      {
-        img: "products/deep-cleanse.jpg",
-        name: "Deep Cleanse",
-        price: "$28",
-      },
-      {
-        img: "products/balancing-essence.jpg",
-        name: "Balancing Essence",
-        price: "$40",
-      },
-      {
-        img: "products/treatment-serum.jpg",
-        name: "Treatment Serum",
+        img: "products/green_plum_toner.jpg",
+        name: "Green Plum Refreshing Toner: AHA + BHA",
         price: "$50",
       },
       {
-        img: "products/gel-moisturizer.jpg",
-        name: "Gel Moisturizer",
-        price: "$38",
+        img: "products/honey_glow_mask.jpg",
+        name: "Ground Rice and Honey Glow Mask",
+        price: "$42",
       },
+      {
+        img: "products/cleansing_balm.jpg",
+        name: "Radiance Cleansing Balm",
+        price: "$26",
+      }
+    ],
+    eveningProducts: [
+      {
+        img: "products/red_bean_mask.jpg",
+        name: "Red Bean Refreshing Pore Mask",
+        price: "$40",
+      },
+      {
+        img: "products/light_on_serum.jpg",
+        name: "Light On Serum Centella + Vita C",
+        price: "$55",
+      },
+      {
+        img: "products/apricot_gel.jpg",
+        name: "Apricot Blossom Peeling Gel",
+        price: "$36",
+      }
     ],
   },
 };
