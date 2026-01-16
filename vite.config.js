4c672ae4e7b2c87f6cefa0ae7792ae4ed6eed4b6
katarina-bajnokova <r0997616@student.thomasmore.be>
2026-01-16 13:43:37 +0100

    cleaned up the files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/beauty-of-joseon/", // 👈 REQUIRED FOR GITHUB PAGES
+  base: "/beauty-of-joseon/",
   plugins: [react()],
   resolve: {
     alias: {
